fb8185467d28606e5aa6dd05dba71ba279e3b164
yooweeng <nyw20001@hotmail.com>
2023-05-23 20:31:15 +0800

    implement customer index and moviedetail page

---
+++ MusicStoreAngularjsMVCApp/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using MusicStoreAngularjsMVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStoreAngularjsMVCApp.Controllers
+{
+    [Authorize(Roles = "Customer")]
+    public class CustomerController : Controller
+    {
+        readonly MusicStoreAppEntities db = new MusicStoreAppEntities();
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MovieDetail(int id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
