255088b0fa6753987c38f861af94fcb7aafe8c8e
yooweeng <nyw20001@hotmail.com>
2023-05-25 10:12:56 +0800

    get id from query param

--- MusicStoreAngularjsMVCApp/Scripts/app/customer/customerMovieDetail.controller.js
+++ MusicStoreAngularjsMVCApp/Scripts/app/customer/customerMovieDetail.controller.js
@@ -2,10 +2,14 @@
 
 .controller('customerMovieDetailCtrl', ['$scope', '$http', function ($scope, $http) {
     // get id
+    address = window.location.search;
+    parameterList = new URLSearchParams(window.location.search);
+    id = parameterList.get('id');
 
     $http({
         method: "GET",
-        url: "/Movies?id=24"
+        url: "/Movies",
+        params: { id: id }
     }).then(function success(response) {
         $scope.movie = response.data.Movie;
     }, function error(response) {
